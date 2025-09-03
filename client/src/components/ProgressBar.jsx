547542ed4c279397f61366719c6a5ea632200bb5
iLuvMelon <dylankane0117@gmail.com>
2025-09-02 20:25:27 -0400

    added confetti

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import Confetti from "react-confetti";
 
 export default function ProgressBar({ target, progress }) {
   const percent =
     target && progress ? (Number(progress) / Number(target)) * 100 : 0;
   return (
     <section class="mt-6 w-4/5">
+      {percent > 100 && <Confetti opacity={0.5}></Confetti>}
       <div class="w-full h-4 rounded-xl bg-gray-500">
         <div
           class="h-full rounded-xl bg-white transition duration-50 ease-in-out"
