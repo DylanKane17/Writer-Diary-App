b6ab80dfd1dae608838c241ea59036b8a5919d78
iLuvMelon <dylankane0117@gmail.com>
2025-09-05 10:19:56 -0400

    PEP8 adjustments

--- server/server.py
+++ server/server.py
@@ -1,10 +1,14 @@
+'''
+This module instantiates Flask to run the backend API.
+It handles user verification and access to the Firebase Realtime Database.
+'''
+
+import os
 import firebase_admin
-from firebase_admin import credentials, db, auth
-import json
-from flask import Flask, jsonify, request, make_response
+from firebase_admin import credentials, db, auth, exceptions
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 CRED_FILE = os.getenv('PATH_TO_CRED')
@@ -15,21 +19,26 @@ CORS(app,
      supports_credentials=True)
 
 cred = credentials.Certificate(CRED_FILE)
-firebase = firebase_admin.initialize_app(cred, {'databaseURL': 'https://firstproject-b2e92-default-rtdb.firebaseio.com'})
+firebase = firebase_admin.initialize_app(cred,
+    {'databaseURL': 'https://firstproject-b2e92-default-rtdb.firebaseio.com'})
 
 
 @app.route('/get-user-data', methods=["POST"])
 def retrieve_user_data():
+    '''Retrieves user data from db'''
     uid = request.json['uid']
     try:
         ref = db.reference(f'/user-data/{uid}')
         user_data = ref.get()
         return jsonify(user_data), 200
-    except:
-        return {"message": "Something went wrong"}, 500
+    except exceptions.PermissionDeniedError:
+        return {"message": "Permission denied"}, 403
+    except Exception as error:
+        return {"message": "Something went wrong:", "error": str(error)}, 500
 
 @app.route('/new-user-data', methods=["POST"])
 def init_user_data():
+    '''Writes data for a new user'''
     uid = request.json['uid']
     print(f"String: {str(uid)}")
     ref = db.reference('/user-data')
@@ -40,14 +49,15 @@ def init_user_data():
 
 @app.route("/verify-user", methods=["POST"])
 def verify_user():
+    '''Verifies user before allowing database modification'''
     id_token = request.json['JWT']
     decoded_token = auth.verify_id_token(id_token)
     uid = decoded_token['uid']
     return jsonify({"uid": uid})
- 
+
 @app.route('/add-user-project', methods=["POST"])
 def add_project():
-    
+    '''Writes data for a new project'''
     to_add = {
         'name': request.json['name'],
         'progress': int(request.json['progress']),
@@ -56,22 +66,20 @@ def add_project():
     }
     uid = request.json['uid']
     user_ref = db.reference(f'/user-data/{uid}')
-    to_add_ref = user_ref.push().set(to_add)
-    print(to_add_ref)
+    user_ref.push().set(to_add)
     return {"message": "OK"}, 200
 
 
 @app.route('/add-user-entry', methods=["POST"])
 def add_entry():
-
+    '''Writes data for a new entry'''
     to_add = {
         'title': request.json['title'],
-        'notes': request.json['notes'], 
-        'date_added': request.json['date'], 
+        'notes': request.json['notes'],
+        'date_added': request.json['date'],
         'progress': request.json['progress']
     }
-    
-    
+
     uid = request.json['uid']
     project = request.json['project']
 
@@ -81,18 +89,18 @@ def add_entry():
     update_progress_ref.set(new_progress)
 
     user_ref = db.reference(f'/user-data/{uid}/{project}/entries')
-    to_add_ref = user_ref.push().set(to_add)
-    print(to_add_ref)
+    user_ref.push().set(to_add)
     return {"message": "OK"}, 200
 
 @app.route('/update-user-data', methods=["POST"])
 def update_data():
+    '''Updates data for a specific entrys'''
     entry_id = request.json['entry_id']
 
     updated_data = {
         'title': request.json['title'],
-        'notes': request.json['notes'], 
-        'date_added': request.json['date'], 
+        'notes': request.json['notes'],
+        'date_added': request.json['date'],
         'progress': request.json['progress']
     }
 
@@ -104,6 +112,7 @@ def update_data():
 
 @app.route('/delete-user-data', methods=["POST"])
 def delete_user_data():
+    '''Deletes user data when their account no longer exists'''
     uid_to_delete = request.json['uid']
     user_ref_to_delete = db.reference(f'/user-data/{uid_to_delete}')
     user_ref_to_delete.delete()
@@ -111,12 +120,12 @@ def delete_user_data():
 
 @app.route('/delete-user-project', methods=["POST"])
 def delete_user_project():
+    '''Deletes a user's project'''
     project = request.json['project']
     uid = request.json['uid']
     project_ref_to_delete = db.reference(f'/user-data/{uid}/{project}')
     project_ref_to_delete.delete()
     return {"message": "project deleted!"}, 200
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
